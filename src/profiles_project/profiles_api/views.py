9eefa350d9fa726b16704dc8e182399da3151adc
the_ghoost <bensebabillel1996@gmail.com>
2019-03-20 23:33:31 +0000

    post put pacth and delete methode to the api methode

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 # Create your views here.
+
+
+serializer_class =serializers.HelloSerializer
 
 class HelloApiView(APIView):
     """Teste API View"""
@@ -20,3 +26,32 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello! Word','an_apiview' : an_apiview })
+
+    def post(self, request):
+        """create hello message with our name. """
+
+        serializer= serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name= serializer.data.get('name')
+            message ='hello {0}'.format(name)
+            return Response ({'message':message})
+
+        else :
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request,pk=None):
+        """handle updating an object"""
+
+        return Response({'methode':'put'})
+
+    def patch(self,request,pk=None):
+        """Patch request, only updates fields provided in the request ."""
+
+        return Response({'methode':'patch'})
+
+    def delete(self,request,pk=None):
+        """Delete an object"""
+
+        return Response({'methode':'delete'})
+
