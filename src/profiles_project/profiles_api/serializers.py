649505aa81196ec7c250dc130a2735222880bd10
the_ghoost <bensebabillel1996@gmail.com>
2019-03-21 21:29:57 +0000

    added provide permissions to use profile feed items API

--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -31,7 +31,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
         return user
 
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Aserializer for profile feed item."""
 
+    class Meta :
+        model = models.ProfileFeedItem
+        fields = ('id','user_profile','status_text','created_on')
+        extra_kwargs = {'user_profile': {'read_only' : True}}
 
-
-        
+         
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -8,6 +8,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
 
 from . import serializers
 from . import models
@@ -131,3 +132,18 @@ class LoginViewSet(viewsets.ViewSet):
         """use the ObtainAuthToken APIView to validate and creat a token,"""
 
         return ObtainAuthToken().post(request)
+
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items."""
+
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.PostOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user."""
+
+        serializer.save(user_profile=self.request.user)
